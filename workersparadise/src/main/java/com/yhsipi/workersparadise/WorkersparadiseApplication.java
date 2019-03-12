38b0c3b237de8f02c9ff9f195ee8073238b2a413
tommy <me@tommyeriksson.nu>
2019-03-12 19:57:51 +0100

    First commit

---
+++ workersparadise/src/main/java/com/yhsipi/workersparadise/WorkersparadiseApplication.java
@@ -0,0 +1,13 @@
+package com.yhsipi.workersparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersparadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkersparadiseApplication.class, args);
+	}
+
+}
