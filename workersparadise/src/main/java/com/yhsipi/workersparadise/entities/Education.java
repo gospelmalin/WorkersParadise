92780acc97a2dfac9d45dedd5773bd7d9dca2132
Malin Albinsson <mea733@telia.com>
2019-04-25 13:35:27 +0200

    Updated entity education after table update

--- workersparadise/src/main/java/com/yhsipi/workersparadise/entities/Education.java
+++ workersparadise/src/main/java/com/yhsipi/workersparadise/entities/Education.java
@@ -14,9 +14,16 @@ import java.util.Date;
 @NamedQuery(name="Education.findAll", query="SELECT e FROM Education e")
 public class Education implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
 	@EmbeddedId
 	private EducationPK id;
+
+	@Column(name="education_description")
+	private String educationDescription;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="education_end_date")
+	private Date educationEndDate;
 
 	@Column(name="education_end_day")
 	private byte educationEndDay;
@@ -27,13 +34,7 @@ public class Education implements Serializable {
 	@Temporal(TemporalType.DATE)
 	@Column(name="education_end_year")
 	private Date educationEndYear;
-	//private byte educationEndYear;
-	
-/*
-	@Temporal(TemporalType.DATE)
-	@Column(name="education_end_date")
-	private Date educationEndDate;
-*/
+
 	@Column(name="education_grade")
 	private String educationGrade;
 
@@ -42,12 +43,6 @@ public class Education implements Serializable {
 
 	@Column(name="education_name")
 	private String educationName;
-	
-	@Column(name="education_description")
-	private String educationDescription;
-	
-	@Column(name="school")
-	private String school;
 
 	@Temporal(TemporalType.DATE)
 	@Column(name="education_start_date")
@@ -64,16 +59,19 @@ public class Education implements Serializable {
 	private Date educationStartYear;
 
 	@Column(name="is_program")
-	private boolean isProgram;
+	private byte isProgram;
+
+	private String school;
 
 	//bi-directional many-to-one association to Person
-		@ManyToOne
-		@JoinColumn(name="id_person",insertable=false, updatable=false)
-		private Person person;
+	@ManyToOne
+	@JoinColumn(name="id_person",insertable=false, updatable=false)
+	private Person person;
+
 	
 	public Education() {
 	}
-
+	
 	public EducationPK getId() {
 		return this.id;
 	}
@@ -82,7 +80,15 @@ public class Education implements Serializable {
 		this.id = id;
 	}
 	
-	/*
+
+	public String getEducationDescription() {
+		return this.educationDescription;
+	}
+
+	public void setEducationDescription(String educationDescription) {
+		this.educationDescription = educationDescription;
+	}
+
 	public Date getEducationEndDate() {
 		return this.educationEndDate;
 	}
@@ -90,7 +96,7 @@ public class Education implements Serializable {
 	public void setEducationEndDate(Date educationEndDate) {
 		this.educationEndDate = educationEndDate;
 	}
-*/
+
 	public byte getEducationEndDay() {
 		return this.educationEndDay;
 	}
@@ -107,12 +113,10 @@ public class Education implements Serializable {
 		this.educationEndMonth = educationEndMonth;
 	}
 
-	//public byte getEducationEndYear() {
 	public Date getEducationEndYear() {
 		return this.educationEndYear;
 	}
 
-	//public void setEducationEndYear(byte educationEndYear) {
 	public void setEducationEndYear(Date educationEndYear) {
 		this.educationEndYear = educationEndYear;
 	}
@@ -173,34 +177,22 @@ public class Education implements Serializable {
 		this.educationStartYear = educationStartYear;
 	}
 
-	public boolean getIsProgram() {
+	public byte getIsProgram() {
 		return this.isProgram;
 	}
 
-	public void setIsProgram(boolean isProgram) {
+	public void setIsProgram(byte isProgram) {
 		this.isProgram = isProgram;
 	}
 
-	public String getEducationDescription() {
-		return educationDescription;
-	}
-
-	public void setEducationDescription(String educationDescription) {
-		this.educationDescription = educationDescription;
-	}
-
 	public String getSchool() {
-		return school;
+		return this.school;
 	}
 
 	public void setSchool(String school) {
 		this.school = school;
 	}
-
-	public void setProgram(boolean isProgram) {
-		this.isProgram = isProgram;
-	}
-
+	
 	public Person getPerson() {
 		return this.person;
 	}
@@ -208,7 +200,6 @@ public class Education implements Serializable {
 	public void setPerson(Person person) {
 		this.person = person;
 	}
-	
-	
+
 
 }
