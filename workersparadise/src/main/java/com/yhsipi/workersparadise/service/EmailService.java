868dc7c78a8203ceae2fc687c324ffbd998196b0
Malin Albinsson <mea733@telia.com>
2019-05-04 17:06:54 +0200

    EmailService added

---
+++ workersparadise/src/main/java/com/yhsipi/workersparadise/service/EmailService.java
@@ -0,0 +1,39 @@
+package com.yhsipi.workersparadise.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yhsipi.workersparadise.entities.Email;
+import com.yhsipi.workersparadise.entities.EmailPK;
+import com.yhsipi.workersparadise.repository.EmailRepository;
+
+@Service
+public class EmailService {
+	
+	@Autowired
+	EmailRepository emailRepository;
+	
+	public List<Email> findAll() {
+		return emailRepository.findAll();
+	}
+	
+	public List<Email> findByPerson(int personId) {
+		return emailRepository.findByPerson(personId);
+	}
+	
+	public Optional<Email> findOne(EmailPK id) {
+		return emailRepository.findById(id);
+	}
+
+	public void saveEmail(Email email) {
+		emailRepository.save(email);
+	}
+
+	public void deleteEmail(int id) {
+		emailRepository.deleteById(id);
+	}
+
+}
