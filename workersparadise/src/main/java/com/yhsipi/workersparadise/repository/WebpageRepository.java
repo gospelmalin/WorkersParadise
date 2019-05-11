6659fa335f5695224462797a9221eeaa49ef2a7e
Malin Albinsson <mea733@telia.com>
2019-05-11 16:10:10 +0200

    WebpageRepository added

---
+++ workersparadise/src/main/java/com/yhsipi/workersparadise/repository/WebpageRepository.java
@@ -0,0 +1,25 @@
+package com.yhsipi.workersparadise.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.yhsipi.workersparadise.entities.Email;
+import com.yhsipi.workersparadise.entities.EmailPK;
+import com.yhsipi.workersparadise.entities.Webpage;
+import com.yhsipi.workersparadise.entities.WebpagePK;
+
+public interface WebpageRepository extends JpaRepository<Webpage, Integer>{
+	
+public final static String FIND_BY_PERSON_QUERY = "SELECT w FROM Webpage w LEFT JOIN w.person p WHERE p.id = :id";
+	
+	// Find emails by person
+	 @Query(FIND_BY_PERSON_QUERY)
+	 public List<Webpage> findByPerson(@Param("id") int id);
+
+	public Optional<Webpage> findById(WebpagePK id);
+
+}
