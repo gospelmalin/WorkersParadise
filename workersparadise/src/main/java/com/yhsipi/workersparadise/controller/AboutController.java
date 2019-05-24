a808f8246fb1633724eeaf1bb3c6b4af4e6a4402
tommy <me@tommyeriksson.nu>
2019-05-25 01:33:08 +0200

    about redigerad

--- workersparadise/src/main/java/com/yhsipi/workersparadise/controller/AboutController.java
+++ workersparadise/src/main/java/com/yhsipi/workersparadise/controller/AboutController.java
@@ -1,20 +1,27 @@
 package com.yhsipi.workersparadise.controller;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yhsipi.workersparadise.entities.About;
+import com.yhsipi.workersparadise.entities.Users;
 import com.yhsipi.workersparadise.service.AboutService;
 import com.yhsipi.workersparadise.service.PersonService;
+import com.yhsipi.workersparadise.service.UserService;
 
 @Controller
 @RequestMapping(value = "/about")
@@ -24,30 +31,64 @@ public class AboutController {
 	private AboutService aboutService;
 	@Autowired
 	private PersonService personService;
+	@Autowired
+	private UserService userService;
 	
-	@GetMapping("/")
-	public String About(Model model) {
+	@GetMapping("/all")
+	public String AboutAll(Model model) {
 		model.addAttribute("abouts", aboutService.findAll());
 		return "about/index";
 	}
 	
+	@GetMapping("/")
+	public String About(Model model) {
+		
+		// Get logged in user
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Users user = userService.findByUsername(authentication.getName());
+		
+		About about = aboutService.findByPerson(user.person.getIdPerson());
+		
+		if (about == null) {
+			about = new About();
+			about.setIdPerson(user.person.getIdPerson());
+		}
+
+		model.addAttribute("about", about);
+		return "about/dashboard";
+	}
+	
 	@GetMapping("/edit/{id}")
 	public String About(@PathVariable int id, Model model) {
-		model.addAttribute("about", aboutService.findOne(id));
-		return "/about/add";
+		
+		// Get logged in user
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Users user = userService.findByUsername(authentication.getName());
+		
+		Optional<About> a = aboutService.findOne(id);
+		
+		if (a.get().getIdPerson() != user.person.getIdPerson())
+			return "redirect:/about/"; 							 	// TODO en redirect till en felsida 
+				
+		model.addAttribute("about", a);
+		return "/about/edit";
 	}
-	
-	@GetMapping("/add")
-	public String add(Model model) {
-		model.addAttribute("about", new About());
-		return "/about/add";
-	}
-	
-	@PostMapping("/add")
+		
+	@PostMapping("/save")
 	public String saveAbout(@Valid About about, BindingResult result, Model model) {
+		
 		about.setPerson(personService.findOne(about.getIdPerson()));
+		
 		if(result.hasErrors()) {
-			return "about/add";
+			// log
+			System.out.println("\n################ Save Erorrs in About ################\n#" + about + "\n# ");
+			
+			// log all errors
+			for (ObjectError error : result.getAllErrors()) {
+				System.out.println("# " + error.toString());
+			} 
+			model.addAttribute("about", about);
+			return "about/edit";
 		}
 		aboutService.saveAbout(about);
 		return "redirect:/about/";
--- workersparadise/src/main/java/com/yhsipi/workersparadise/repository/AboutRepository.java
+++ workersparadise/src/main/java/com/yhsipi/workersparadise/repository/AboutRepository.java
@@ -1,8 +1,15 @@
 package com.yhsipi.workersparadise.repository;
 
 import com.yhsipi.workersparadise.entities.About;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AboutRepository extends JpaRepository<About, Integer> {
 
+	public final static String FIND_BY_PERSON_QUERY = "SELECT DISTINCT a FROM About a WHERE a.idPerson = :id";
+
+	@Query(FIND_BY_PERSON_QUERY)
+	public About findByPerson(@Param("id") int id);
 }
