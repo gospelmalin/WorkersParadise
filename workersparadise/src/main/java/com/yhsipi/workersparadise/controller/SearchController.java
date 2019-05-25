8ec0264c6b9de44faea27490b8023e7a245f59c9
Sten Karlsson <koffov@gmail.com>
2019-05-25 17:03:48 +0200

    sökfält på första sidan är kopplad till controller och funkar

--- workersparadise/src/main/java/com/yhsipi/workersparadise/controller/SearchController.java
+++ workersparadise/src/main/java/com/yhsipi/workersparadise/controller/SearchController.java
@@ -2,14 +2,12 @@ package com.yhsipi.workersparadise.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.yhsipi.workersparadise.entities.Competence;
 import com.yhsipi.workersparadise.entities.PersonCompetence;
@@ -24,16 +22,9 @@ public class SearchController {
 	private PersonCompetenceService personCompetenceService;
 	@Autowired
 	private CompetenceService competenceService;
-	
-	@GetMapping("/")
-	public String displaySearchResults(@Valid String searchStr,Model model) {
-		return "/search/" + searchStr;
-	}
-	
-	
-	@RequestMapping(value = "/{searchStr}")
-	public String getSearchResult(@PathVariable String searchStr, Model model) {
-		
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String getSearchResult(@RequestParam("searchStr") String searchStr, Model model) {
 		List<Competence> c = competenceService.findByCompetenceName(searchStr);
 		int competenceId = c.get(0).getIdCompetence();
 		List<PersonCompetence> pc = personCompetenceService.findByCompetence(competenceId);
